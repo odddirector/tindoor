c000c425d10a545bc3cdc8a4d25dfdb708c92174
odddirector <odd@odddirector.com>
2023-09-04 22:25:08 +0200

    Use react-app dotenv

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -1,30 +1,16 @@
 import { initializeApp } from 'firebase/app'
 import { getStorage } from 'firebase/storage'
 import { getAuth } from "firebase/auth";
-import keys from '../config/keys'
-// import 'dotenv/config'
 
-// const firebaseConfig = {
-//   apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.PUBLIC_FIREBASE_APP_ID,
-// }
-
-
-// Create your own keys.js file with a keys object in it containing firebase keys 
-// config/keys.js is excluded in .gitignore 
-// (had problems with dotenv, didn't have time to resolve)
 const firebaseConfig = {
-    apiKey: keys.apiKey,
-    authDomain: keys.authDomain,
-    projectId: keys.projectId,
-    storageBucket: keys.storageBucket,
-    messagingSenderId: keys.messagingSenderId,
-    appId: keys.appId,
-  }
+  apiKey: process.env.REACT_APP_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PUBLIC_FIREBASE_APP_ID,
+}
+
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
